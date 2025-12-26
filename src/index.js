655a771cd3a4f80aa19bf1c5625364da7513e3ad
Bmisi70 <benyomisi@icloud.com>
2025-12-26 10:17:11 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import IntroLandingV2 from './App.js';
 
-import IntroLandingV2 from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <IntroLandingV2 />
   </StrictMode>
